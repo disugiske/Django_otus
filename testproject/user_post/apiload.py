ae6c1b550c84e03e3bd4d847bbffbaeca2ec20b3
disugiske <disugiske@yandex.ru>
2022-06-27 14:50:21 +0300

    create django

--- testproject/user_post/apiload.py
+++ testproject/user_post/apiload.py
@@ -6,31 +6,34 @@ from models import Usersjson, Postsjson
 USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
 POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
 
-qweryset = Usersjson.object.all()
-if qweryset.exists() == False:
-    def get_userdata():
-        userdata = requests.get(USERS_DATA_URL).json()
-        return userdata
 
-    def get_posts():
-        postdata = requests.get(POSTS_DATA_URL).json()
-        return postdata
+def get_userdata():
+    userdata = requests.get(USERS_DATA_URL).json()
+    return userdata
+
+def get_posts():
+    postdata = requests.get(POSTS_DATA_URL).json()
+    return postdata
 
 
-    def load_json_users(userdata, postdata):
-        for list_data in userdata:
-            user_json.append(Usersjson(
-                name=list_data.get("name"),
-                username=list_data.get("username"),
-                email=list_data.get("email"),
-                user_id=list_data.get("id"),
-                                    )
-            )
-        Usersjson.object.bulk_create(user_json)
+def load_json_users(userdata, postdata):
+    for list_data in userdata:
+        user_json.append(Usersjson(
+            name=list_data.get("name"),
+            username=list_data.get("username"),
+            email=list_data.get("email"),
+            user_id=list_data.get("id"),
+                                )
+        )
+    Usersjson.object.bulk_create(user_json)
 
-        for post_data in postdata:
-            post_json.append(Postsjson(
-                user_id=post_data.get("userId"),
-                body=post_data.get("body"))
-            )
-        Postsjson.object.bulk_create(post_json)
+    for post_data in postdata:
+        post_json.append(Postsjson(
+            user_id=post_data.get("userId"),
+            body=post_data.get("body"))
+        )
+    Postsjson.object.bulk_create(post_json)
+
+def init():
+    if not Usersjson.object.all().exists():
+        load_json_users(get_userdata(), get_posts())
